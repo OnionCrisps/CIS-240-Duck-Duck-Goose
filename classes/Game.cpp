19d1f12469ebb28f2d2d365783414e7d54c8636f
OnionCrisps <gavin.harban@gmail.com>
2026-04-08 12:52:43 -0600

    Structuring the Game class, need to add documentation.

--- classes/Game.cpp
+++ classes/Game.cpp
@@ -1,7 +1,32 @@
 #include "Game.h"
 
 Game::Game()
-{}
+{
+
+}
 
 Game::~Game()
 {}
+
+
+void Game::buildMenu()
+{
+	push_toOptions({ "Start", "Rules", "Quit" });
+	push_Functions({
+		[this]() {createCircle(); play(); },
+		[this]() {},
+		[this]() {exit(0); }});
+	IBuilder::buildMenu();
+}
+
+Game::Player Game::createPlayer(std::string name)
+{
+	Player player;
+	player.playerID = circle.getSize(); //assigned before the creation
+	player.isEliminated = false;
+
+	player.name = name;
+
+	return player;
+}
+
--- classes/Game.h
+++ classes/Game.h
@@ -1,37 +1,48 @@
 #pragma once
 #include "../classes/IBuilder.h"
 #include "../classes/DCList.h"
-class Game {
-public:
-	Game();
-	~Game();
-	//system to build the menu.
-	//system to get players.
-	//system to make players.
-	//system to assign players a role.
-	//system to make the circle.
-	//system to render the circle.
-	//system to make the cooking pot.
-	//system to render the cooking pot.
-	/* Main Game Rules/Functionality
-	 * choose the "it"
-	 * have the "it" pick someone (duck duck duck goose)
-	 * anticlockwise or clockwise choice
-	 * The tapper must tap a key to run around the ring (quick time event)
-	 * If they don't tap enough, then the "it" catches them and they get put in the cooking pot
-	 * 
-	*/
-private:
-	enum Role //storage structure for each type
-	{
-		TAPPER,
-		GOOSE,
-		IT
-	};
 
-	struct Player {
-		Role status;
-		int listIndex;
-		bool isEliminated = false;
-	};
+class Game : public IBuilder {
+private:
+    enum Role
+    {
+        TAPPER,
+        GOOSE,
+        IT
+    };
+
+    struct Player {
+        std::string name;
+        Role status;
+        int playerID; //index in list; no method for setting id is needed as it is set upon creation and removal
+        bool isEliminated = false;
+    };
+
+    unsigned int round;
+    DCList circle;
+    std::vector<Player> cookingPot;
+    int itIndex; // tracks who is "it" in the circle
+
+public:
+    Game();
+    ~Game();
+
+    void buildMenu();
+
+    Player createPlayer(std::string name);
+
+    const Player& getPlayer(int id) const;
+
+    void setRole(Player& player, Role role);
+
+    void createCircle();
+
+    void renderCircle() const;
+
+    void addToPot(const Player& player);
+
+    void renderPot() const;
+
+    // main game loop
+    void play();
 };
