3e5a01ee1922b4b32912e073e4c8f07e3bd2c552
OnionCrisps <gavin.harban@gmail.com>
2026-04-18 23:43:20 -0600

    Fixed DCList

--- classes/DCList.cpp
+++ classes/DCList.cpp
@@ -1,5 +1,50 @@
 #include "DCList.h"
 
+DCList::DCList()
+{
+    first = nullptr;
+    size = 0;
+}
+
+DCList::DCList(const DCList& source) {
+    first = nullptr;
+    size = 0;
+    if (source.isEmpty()) return;
+    for (int i = 0; i < source.size; i++) {
+        insert(source.peek(i), size); //add to the end
+    }
+}
+
+DCList::~DCList()
+{
+    while (!isEmpty()) {
+        remove(0);
+    }
+}
+
+int DCList::getSize() const
+{
+    return size;
+}
+
+bool DCList::isEmpty() const
+{
+    return(size == 0);
+}
+
+bool DCList::isFull() const
+{
+    {
+        Node* temp = new Node(T());
+        if (temp == nullptr) return true;
+        else
+        {
+            delete temp;
+            return false;
+        }
+    }
+
+}
 T DCList::peek(int index) const
 {
 	if (isEmpty()) return T();
@@ -207,55 +252,4 @@ DCList::Node* DCList::retrieveAt(int index) const
 	return ptr;
 }
 
-DCList::DCList()
-{
-	first = nullptr;
-	size = 0;
-
-}
-
-DCList::DCList(const DCList&source)
-{
-	first = nullptr;
-	size = 0;
-
-	if (source.isEmpty()) return;
-
-	for (int i = 0; i < source.size; i++) {
-		insert(source.peek(i), i);
-	}
-
-
-}
-
-DCList::~DCList()
-{
-	while (!isEmpty()) {
-		remove(0);
-	}
-}
-
-int DCList::getSize() const
-{
-	return size  ;
-}
-
-bool DCList::isEmpty() const
-{
-	return(size == 0);
-}
-
-bool DCList::isFull() const
-{
-	{
-		Node* temp = new Node(T());
-		if (temp == nullptr) return true;
-		else
-		{
-			delete temp;
-			return false;
-		}
-	}
-
-}
 
